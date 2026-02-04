f55c8c97bc0e6637719b23ec66fe3ec738964d50
mateus pereira <mateusinhopereira2010@hotmail.com>
2026-02-04 18:50:19 +0000

    ADD handlers

---
+++ BlueBerryFinance/BlueBerryFinance/Application/Handlers/Interfaces/IRequestHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace BlueBerryFinance.API.Application.Handlers.Interfaces
+{
+    public interface IRequestHandler<TRequest, TResponse>
+    {
+        Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+    }
+}
