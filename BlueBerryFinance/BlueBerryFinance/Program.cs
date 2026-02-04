4a83a62906281e5c646276612073c5f3cb8bc306
mateus pereira <mateusinhopereira2010@hotmail.com>
2026-02-04 18:16:19 +0000

    proj config

--- BlueBerryFinance/BlueBerryFinance/Program.cs
+++ BlueBerryFinance/BlueBerryFinance/Program.cs
@@ -1,10 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using BlueBerryFinance.API.Infrastructure.Utils.Agents.Factories;
+using BlueBerryFinance.API.Infrastructure.Utils.Agents.Factories.Interfaces;
+using BlueBerryFinance.API.Infrastructure.Utils.Agents.Finance.Interfaces;
+using BlueBerryFinance.API.Infrastructure.Utils.Agents.GeneralPurpose;
+using BlueBerryFinance.API.Infrastructure.Utils.Agents.Helpers;
+using BlueBerryFinance.API.Infrastructure.Utils.Agents.Helpers.Interfaces;
+using BlueBerryFinance.API.Infrastructure.Utils.Agents.Models;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Configuration
+builder.Services.Configure<AIAgentOptions>(builder.Configuration.GetSection("OpenAI"));
+
+// Infrastructure
+builder.Services.AddSingleton<IPromptLoader, PromptLoader>();
+builder.Services.AddSingleton<IAIAgentFactory, AIAgentFactory>();
+
+// Agents
+builder.Services.AddScoped<IFinantialAssistantAgent, FinantialAssistantAgent>();
 
 var app = builder.Build();
 
