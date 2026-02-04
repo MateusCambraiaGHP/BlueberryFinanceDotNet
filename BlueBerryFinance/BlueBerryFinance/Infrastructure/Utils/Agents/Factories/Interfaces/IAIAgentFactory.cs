a134072c104fd80725b33ecf2000653dbd7df3b5
mateus pereira <mateusinhopereira2010@hotmail.com>
2026-02-04 18:14:22 +0000

    add agent factory

--- BlueBerryFinance/BlueBerryFinance/Infrastructure/Utils/Agents/Factories/Interfaces/IAIAgentFactory.cs
+++ BlueBerryFinance/BlueBerryFinance/Infrastructure/Utils/Agents/Factories/Interfaces/IAIAgentFactory.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.Agents.AI;
+﻿using BlueBerryFinance.API.Infrastructure.Utils.Agents.Enums;
+using Microsoft.Agents.AI;
 
 namespace BlueBerryFinance.API.Infrastructure.Utils.Agents.Factories.Interfaces
 {
     public interface IAIAgentFactory
     {
-        AIAgent Create(string instructions, string? model = null);
+        AIAgent Create(
+            string instructions, 
+            string model = OpenAIModels.Gpt41, 
+            AIProvider provider = AIProvider.OpenAI);
     }
 }
