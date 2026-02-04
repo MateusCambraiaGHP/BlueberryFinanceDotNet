62a10054ddfcca74505ec701cda74d1f39ec248c
mateus pereira <mateusinhopereira2010@hotmail.com>
2026-02-04 18:52:19 +0000

    add swagger

--- BlueBerryFinance/BlueBerryFinance/Program.cs
+++ BlueBerryFinance/BlueBerryFinance/Program.cs
@@ -1,16 +1,21 @@
-using BlueBerryFinance.API.Infrastructure.Utils.Agents.Factories;
-using BlueBerryFinance.API.Infrastructure.Utils.Agents.Factories.Interfaces;
+using BlueBerryFinance.API.Application.Handlers;
+using BlueBerryFinance.API.Application.Handlers.Interfaces;
 using BlueBerryFinance.API.Infrastructure.Utils.Agents.Finance.Interfaces;
 using BlueBerryFinance.API.Infrastructure.Utils.Agents.GeneralPurpose;
-using BlueBerryFinance.API.Infrastructure.Utils.Agents.Helpers;
-using BlueBerryFinance.API.Infrastructure.Utils.Agents.Helpers.Interfaces;
-using BlueBerryFinance.API.Infrastructure.Utils.Agents.Models;
+using BlueBerryFinance.API.Infrastructure.Utils.Factories;
+using BlueBerryFinance.API.Infrastructure.Utils.Factories.Interfaces;
+using BlueBerryFinance.API.Infrastructure.Utils.Helpers;
+using BlueBerryFinance.API.Infrastructure.Utils.Helpers.Interfaces;
+using BlueBerryFinance.API.Infrastructure.Utils.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
+
 
 // Configuration
 builder.Services.Configure<AIAgentOptions>(builder.Configuration.GetSection("OpenAI"));
@@ -22,12 +27,16 @@ builder.Services.AddSingleton<IAIAgentFactory, AIAgentFactory>();
 // Agents
 builder.Services.AddScoped<IFinantialAssistantAgent, FinantialAssistantAgent>();
 
+//Services
+builder.Services.AddScoped<ITransactionsHandler, TransactionsHandler>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
