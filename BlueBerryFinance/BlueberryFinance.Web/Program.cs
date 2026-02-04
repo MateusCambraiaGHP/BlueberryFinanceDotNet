fa57b581c5fdce271d5e9d869e8e6bb91da6276a
mateus pereira <mateusinhopereira2010@hotmail.com>
2026-02-04 15:58:44 +0000

    add MudBlazor

--- BlueBerryFinance/BlueberryFinance.Web/Program.cs
+++ BlueBerryFinance/BlueberryFinance.Web/Program.cs
@@ -1,10 +1,12 @@
 using BlueberryFinance.Web.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
@@ -15,7 +17,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
